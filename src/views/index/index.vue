2ff2904b400fda39e6f163a2190cbc8d58a3d830
shangsheng <2864175199@qq.com>
2020-10-16 17:07:20 +0800

    第一次提交

---
+++ src/views/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="amap">
+        <el-amap class="amap-demo" vid="amapDemo" :zoom="zoom" >
+        </el-amap>
+    </div>
+</template>
+<script>
+export default {
+    name:'Index',
+    data(){
+        return{
+            zoom:16,
+            //center:[116.397428, 39.90923]中心点:center="center"
+        }
+    }
+}
+</script>
+<style lang="scss">
+#amap{
+    height: 100vh;
+}
+</style>
